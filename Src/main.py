90fbc5eec018d8d9b175b7ece26327856fdbf743
Rahul <rsingh@pcs.com.np>
2021-10-23 19:24:15 +0545

    added lib for converting files to exe

--- Src/main.py
+++ Src/main.py
@@ -7,16 +7,16 @@ from kivy.uix.button import Button
 from kivy.uix.stacklayout import StackLayout
 from kivy.uix.widget import Widget
 
-
 class StackLayoutExample(StackLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        for i in range(0, 10):
+        # self.orientation = "lr-bt"
+        for i in range(0, 100):
             # size = dp(100) + i*10
             size = dp(100)
             # b = Button(text=str(i+1), size_hint=(.2, .2))
             # b = Button(text=str(i+1), size_hint=(None, None), size=(size, size))
-            b = Button(text=str(i+1), size_hint=(None, None), size=(dp(100), dp(60)))
+            b = Button(text=str(i+1), size_hint=(None, None), size=(size, size))
             self.add_widget(b)
 
 
---
+++ my-env.ps1
@@ -0,0 +1,3 @@
+cd Scripts
+activate
+cd ../Src
