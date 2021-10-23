a875cda9bc819b4d28a39f360464f2bf55417cbd
Rahul <rsingh@pcs.com.np>
2021-10-23 20:33:05 +0545

    added custom fonts and adding interactions to the widget

--- Src/main.py
+++ Src/main.py
@@ -1,11 +1,36 @@
 from kivy.app import App
 from kivy.metrics import dp
+from kivy.properties import StringProperty, BooleanProperty
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 # from kivy.uix.gridlayout import GridLayout
 from kivy.uix.stacklayout import StackLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.widget import Widget
+
+
+class WidgetsExample(GridLayout):
+    my_text = StringProperty("Hello!!")
+    count_enabled = BooleanProperty(False)
+    counter = 0
+
+    def on_button_click(self, widget):
+        if self.count_enabled:
+            self.counter += 1
+            self.my_text = "Clicked " + str(self.counter) + " times!!"
+
+    def on_toogle_button_state(self, widget):
+        if widget.state == "normal":
+            widget.text = "OFF"
+            self.count_enabled = False
+        else:
+            widget.text = "ON"
+            self.count_enabled = True
+
+    def on_switch_active(self, widget):
+        print("Switch " + str(widget.active))
+
 
 class StackLayoutExample(StackLayout):
     def __init__(self, **kwargs):
@@ -16,7 +41,8 @@ class StackLayoutExample(StackLayout):
             size = dp(100)
             # b = Button(text=str(i+1), size_hint=(.2, .2))
             # b = Button(text=str(i+1), size_hint=(None, None), size=(size, size))
-            b = Button(text=str(i+1), size_hint=(None, None), size=(size, size))
+            b = Button(text=str(i+1), size_hint=(None, None),
+                       size=(size, size))
             self.add_widget(b)
 
 
@@ -44,6 +70,10 @@ class MainWidget(Widget):
 
 
 class TheLabApp(App):
+    pass
+
+
+class TheLab2App(App):
     pass
 
 
