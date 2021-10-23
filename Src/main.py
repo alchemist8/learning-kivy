544160129d480d5ae706f13e83ddb46c972c78b5
alchemist8 <83078507+alchemist8@users.noreply.github.com>
2021-10-23 06:59:46 +0545

    stack layout

--- Src/main.py
+++ Src/main.py
@@ -1,9 +1,23 @@
 from kivy.app import App
+from kivy.metrics import dp
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 # from kivy.uix.gridlayout import GridLayout
+from kivy.uix.stacklayout import StackLayout
 from kivy.uix.widget import Widget
+
+
+class StackLayoutExample(StackLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        for i in range(0, 10):
+            # size = dp(100) + i*10
+            size = dp(100)
+            # b = Button(text=str(i+1), size_hint=(.2, .2))
+            # b = Button(text=str(i+1), size_hint=(None, None), size=(size, size))
+            b = Button(text=str(i+1), size_hint=(None, None), size=(dp(100), dp(60)))
+            self.add_widget(b)
 
 
 # class GridLayoutExample(GridLayout):
